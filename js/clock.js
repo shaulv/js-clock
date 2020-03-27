4c864efb6b75a50e5ff70f1157e20317c5e56042
shaulv <shaul.vainblat@gmail.com>
2020-03-27 20:29:24 +0300

    js clock object

---
+++ js/clock.js
@@ -0,0 +1,66 @@
+var clock = (() => {
+    function Clock(container) {
+        Clock.prototype.templateHtml(container);
+    }
+    Clock.prototype = {
+        DOM: {
+            getHr: () => document.querySelector(".clock__numbers .hours"),
+            getMin: () => document.querySelector(".clock__numbers .minutes"),
+            getSec: () => document.querySelector(".clock__numbers .seconds")
+        },
+        date: {
+            object: () => new Date(),
+            getHr: () => {
+                let hr = Clock.prototype.date.object().getHours();
+                return Clock.prototype.addZeroBefore(hr) + hr;
+            },
+            getMin: () => {
+                let min = Clock.prototype.date.object().getMinutes();
+                return Clock.prototype.addZeroBefore(min) + min;
+            },
+            getSec: () => {
+                let sec = Clock.prototype.date.object().getSeconds();
+                return Clock.prototype.addZeroBefore(sec) + sec;
+            }
+        },
+        addZeroBefore: (timeNum) => timeNum < 10 ? '0' : '',
+        templateHtml: (container) => {
+            let html = '<section class="clock">' +
+                '<div class="clock__numbers">' +
+                '<span class="hours">%hours%</span>' +
+                '</div>' +
+                '<span class="clock__colon">:</span>' +
+                '<div class="clock__numbers">' +
+                '<span class="minutes">%minutes%</span>' +
+                '</div>' +
+                '<span class="clock__colon">:</span>' +
+                '<div class="clock__numbers">' +
+                '<span class="seconds">%seconds%</span>' +
+                '</div>' +
+                '</section>'
+            html = html.replace("%hours%", Clock.prototype.date.getHr());
+            html = html.replace("%minutes%", Clock.prototype.date.getMin());
+            html = html.replace("%seconds%", Clock.prototype.date.getSec());
+            document.querySelector(container).insertAdjacentHTML('beforeend', html);
+        },
+        updateClock: () => {
+            Clock.prototype.DOM.getHr().innerHTML = Clock.prototype.date.getHr();
+            Clock.prototype.DOM.getMin().innerHTML = Clock.prototype.date.getMin();
+            Clock.prototype.DOM.getSec().innerHTML = Clock.prototype.date.getSec();
+        },
+        clockInterval: null,
+        startAutoUpdateClock: () => {
+            Clock.prototype.clockInterval = setInterval(Clock.prototype.updateClock, 1000);
+        },
+        stopAutoUpdateClock: () => clearInterval(Clock.prototype.clockInterval)
+    }
+
+    return {
+        init: () => {
+            let clock = new Clock("main");
+            clock.startAutoUpdateClock();
+        }
+    }
+})();
+
+clock.init();
